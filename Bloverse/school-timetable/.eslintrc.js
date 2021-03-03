469c8df9815d1f018e1eaf19d1733c46e9f45e9b
derinsola16 <aderinsolaodusany@gmail.com>
2021-03-03 13:31:45 +0100

    first commit

---
+++ Bloverse/school-timetable/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      'plugin:vue/recommended'
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      'no-unused-vars': 'off',
+      'vue/html-indent': 'off',
+      'vue/html-quotes': 'off',
+      'vue/order-in-components': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/multiline-html-element-content-newline': 'off',
+      'vue/html-self-closing': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/attributes-order': 'off',
+    }
+  }
